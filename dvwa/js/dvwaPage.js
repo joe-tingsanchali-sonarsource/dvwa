6ace2c0314e2ceffb2fe805887a384679a94b5c7
jtingsanchali <70384906+joe-tingsanchali-sonarsource@users.noreply.github.com>
2023-03-06 16:27:28 -0600

    Update dvwaPage.js

--- dvwa/js/dvwaPage.js
+++ dvwa/js/dvwaPage.js
@@ -2,6 +2,7 @@
 
 function popUp(URL) {
 	i = 0;
+	j = 0;
 	day = new Date();
 	id = day.getTime();
 	window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width=800,height=300,left=540,top=250');
